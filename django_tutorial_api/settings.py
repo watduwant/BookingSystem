c809395adc7a28ee70ce066e3d9bb8d3a4dcdb1e
shubham bairagi <sbairagi825@gmail.com.com>
2021-07-16 21:14:09 +0530

    initial commit

--- django_tutorial_api/settings.py
+++ django_tutorial_api/settings.py
@@ -180,28 +180,28 @@ REST_USE_JWT = True
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if DEBUG:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.mysql',
-                'NAME': config('DB_NAME'),
-                'USER': config('DB_USER'),
-                'PASSWORD': config('DB_PASSWORD'),
-                'HOST': config('DB_HOST'),
-                'PORT': config('DB_PORT'),
-                'OPTIONS': {
-                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                    'charset': 'utf8mb4',
-                }
-            }
-        }
+}
+# else:
+#     DATABASES = {
+#             'default': {
+#                 'ENGINE': 'django.db.backends.mysql',
+#                 'NAME': config('DB_NAME'),
+#                 'USER': config('DB_USER'),
+#                 'PASSWORD': config('DB_PASSWORD'),
+#                 'HOST': config('DB_HOST'),
+#                 'PORT': config('DB_PORT'),
+#                 'OPTIONS': {
+#                     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#                     'charset': 'utf8mb4',
+#                 }
+#             }
+#         }
 
 
 
