8cc5d700d029d2d64a9b1e550fc13220e0f3c561
Zain zahid <zain.zahid@cruv.org>
2022-01-19 23:58:46 +0530

    created pathology related models and registered them in admin

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,15 @@
+from pathlib import Path
 from django.contrib import admin
-from .models import Shop, Doctor, Service, ServiceDetailsDay,ServiceDetailsDayTime
+from .models import Shop, Doctor, Service, ServiceDetailsDay,ServiceDetailsDayTime, Pathological_Test, Pathological_Test_Service, Phlebotomist, Order, OrderService, ShippingAddress 
 
 # Register your models here.
 admin.site.register(Shop)
 admin.site.register(Doctor)
 admin.site.register(Service)
 admin.site.register(ServiceDetailsDay)
-admin.site.register(ServiceDetailsDayTime)
+admin.site.register(Pathological_Test)
+admin.site.register(Pathological_Test_Service)
+admin.site.register(Phlebotomist)
+admin.site.register(Order)
+admin.site.register(OrderService)
+admin.site.register(ShippingAddress)
--- store/models.py
+++ store/models.py
@@ -45,6 +45,28 @@ class Shop(models.Model):
         super(Shop, self).save(*args, **kwargs)
 
 
+class Pathological_Test(models.Model):
+    TestName = models.CharField(max_length=100)
+    Desc =  models.TextField()
+    Image = models.ImageField(upload_to='shops', blank=True, null=True)
+    SampleType  = models.CharField(max_length=100)
+    FastingRequirement = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.TestName
+
+
+class Pathological_Test_Service(models.Model):
+    Shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="PathologicalTestServices")
+    Tests = models.ForeignKey(Pathological_Test, on_delete=models.CASCADE, related_name="PathologicalTestServices")
+    Price = models.IntegerField()
+    ReportDeliveryTime = models.TimeField()
+
+    def __str__(self):
+        return f"{str(self.Shop)} - {str(self.Tests)}"
+    
+
+
 class Doctor(models.Model):
     Name = models.CharField(max_length=100, unique=True)
     Specialization = models.CharField(max_length=200, blank=False)
@@ -53,6 +75,54 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.Name
+
+class Phlebotomist(models.Model):
+    Shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="Phlebotomists")
+    Name = models.CharField(max_length=100)
+    PhoneNumber = models.CharField(max_length=15)
+
+
+class Order(models.Model):
+    User =  models.OneToOneField(User, on_delete=models.CASCADE, related_name="Order")
+    DateOrdered = models.DateField()
+    complete = models.BooleanField(default=False)
+    transactionId = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.User + " - Order" 
+
+class OrderService(models.Model):
+    Order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="orderServices")
+    PathologicalTestService = models.ForeignKey(Pathological_Test_Service, on_delete=models.CASCADE, related_name="pathologicalTestServices")
+    DateAdded = models.DateField(auto_now_add=True)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.Order + " - " + self.quantity
+
+Gender_Choices  = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+)
+
+class ShippingAddress(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shippingAddresses")
+    patientName = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(choices=Gender_Choices, max_length=5)
+    FlatName = models.CharField(max_length=200)
+    StreetName = models.CharField(max_length=200)
+    pincode =  models.CharField(max_length=10)
+    AddressType = models.CharField(max_length=200)
+    MobileNumber = models.CharField(max_length=15)
+    AppointmentDate = models.DateField()
+    AppointmentTimeSlot = models.TimeField()
+
+    def __str__(self):
+        return self.User + " - " + self.patientName
+    
+
 
 
 class Service(models.Model):
@@ -75,7 +145,7 @@ class Service(models.Model):
 
 class ServiceDetailsDay(models.Model):
     ServiceID = models.ForeignKey(
-        Service, on_delete=models.CASCADE, null=True, blank=True)
+        Service, on_delete=models.CASCADE, null=True, blank=True, related_name='serviceDetailsDays')
     Day = models.CharField(max_length=10, choices=week_days, null=True)
 
     class Meta:
@@ -91,7 +161,7 @@ class ServiceDetailsDay(models.Model):
 
 class ServiceDetailsDayTime(models.Model):
     ServiceDetailsDayID = models.ForeignKey(
-        ServiceDetailsDay, on_delete=models.CASCADE, null=True, blank=True)
+        ServiceDetailsDay, on_delete=models.CASCADE, null=True, blank=True, related_name='serviceDetailsDayTimes')
     Time = models.TimeField()
     Visit_capacity = models.IntegerField()
 
