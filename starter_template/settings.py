7437c04aa1f15c03b0649ee423be1bd0adea6df7
sbairagi <sbairagi825@gmail.com>
2022-05-22 17:21:39 +0530

    setup is complited for android application

--- starter_template/settings.py
+++ starter_template/settings.py
@@ -92,6 +92,7 @@ FCM_DJANGO_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -308,8 +309,8 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+# STATIC_ROOT = 'staticfiles'
 
 
 MESSAGE_TAGS = {
