0511289bbaf68268a2db23a73c74cde32611cd5f
sbairagi <sbairagi825@gmail.com>
2022-05-22 17:56:10 +0530

    adding WhiteNoiseMiddleware in settings.py

--- starter_template/settings.py
+++ starter_template/settings.py
@@ -322,5 +322,7 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
